b711d378f32254dff21836bbe2b1a1914a5edd9e
John Ryan <northdacoder@gmail.com>
2015-02-02 12:58:06 -0600

    add external & internal script slides

---
+++ external.js
@@ -0,0 +1,10 @@
+// This is a JavaScript file!
+
+document.ready(function{
+
+    console.log(document.URL);
+    console.log(document.doctype);
+
+    alert("The external JavaScript file external.js has been successfully loaded into your html page!");
+
+});
